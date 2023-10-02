8358272e07549b073477b8f90e97b234be80c25c
fran0liveira <francisco.leonardo11@outlook.com>
2023-10-02 15:35:07 -0300

    removendo alguns campos

--- projetoteste/src/curso/java/alex/Pessoa.java
+++ projetoteste/src/curso/java/alex/Pessoa.java
@@ -3,11 +3,6 @@ package curso.java.alex;
 public class Pessoa {
 	
 	private String nome;
-	private String sobrenome;
-	private String dataNasc;
-	private String dataNasc2;
-	
-	String ignoreCriado;
 	
 	
 	
@@ -16,24 +11,6 @@ public class Pessoa {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-	public String getSobrenome() {
-		return sobrenome;
-	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-	public String getDataNasc() {
-		return dataNasc;
-	}
-	public void setDataNasc(String dataNasc) {
-		this.dataNasc = dataNasc;
-	}
-	public String getDataNasc2() {
-		return dataNasc2;
-	}
-	public void setDataNasc2(String dataNasc2) {
-		this.dataNasc2 = dataNasc2;
 	}
 
 }
